a937e9b8b472d102db8dd124cbd9894bf8130311
nevvi <jonas.hedeng@gmail.com>
2016-11-22 14:17:43 +0100

    MySQL test

--- main.py
+++ main.py
@@ -1,53 +1,79 @@
+# Copyright 2013 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Sample App Engine application demonstrating how to connect to Google Cloud SQL
+using App Engine's native unix socket or using TCP when running locally.
+
+For more information, see the README.md.
+"""
+
+# [START all]
+import os
+
+import MySQLdb
 import webapp2
 
-from flask import Flask, render_template
-from google.appengine.api import users
 
-app = Flask(__name__)
-app.config['DEBUG'] = True
+# These environment variables are configured in app.yaml.
+CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
 
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
+
+def connect_to_cloudsql():
+    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
+    # will be set to 'Google App Engine/version'.
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        # Connect using the unix socket located at
+        # /cloudsql/cloudsql-connection-name.
+        cloudsql_unix_socket = os.path.join(
+            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+
+        db = MySQLdb.connect(
+            unix_socket=cloudsql_unix_socket,
+            user=CLOUDSQL_USER,
+            passwd=CLOUDSQL_PASSWORD)
+
+    # If the unix socket is unavailable, then try to connect using TCP. This
+    # will work if you're running a local MySQL server or using the Cloud SQL
+    # proxy, for example:
+    #
+    #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
+    #
+    else:
+        db = MySQLdb.connect(
+            host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+
+    return db
+
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        if user:
-            nickname = user.nickname()
-            logout_url = users.create_logout_url('/')
-            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
-                nickname, logout_url)
-        else:
-            login_url = users.create_login_url('/')
-            greeting = '<a href="{}">Sign in</a>'.format(login_url)
+        """Simple request handler that shows all of the MySQL variables."""
+        self.response.headers['Content-Type'] = 'text/plain'
 
-        self.response.write(
-            '<html><body>{}</body></html>'.format(greeting))
+        db = connect_to_cloudsql()
+        cursor = db.cursor()
+        cursor.execute('SHOW VARIABLES')
 
-@app.route('/')
-@app.route('/index/')
-def index():
-    """Return a friendly HTTP greeting."""
-    return render_template('index1.html')
-
-@app.route('/profile/')
-def profile():
-    profile = None
-    return render_template('profile.html', profile=profile)
-
-@app.route('/competition/')
-def competition():
-    return render_template('comp.html')
-
-@app.route('/login/')
-def login():
-    return render_template('login.html')
+        for r in cursor.fetchall():
+            self.response.write('{}\n'.format(r))
 
 
-@app.route('/leaderboard/')
-def leaderboard():
-    return render_template('leaderboard.html')
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
 
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
+# [END all]
